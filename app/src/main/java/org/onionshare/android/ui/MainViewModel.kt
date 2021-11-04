a9973e79a51f0f60fc21a073d66c363d46b53109
Torsten Grote <t@grobox.de>
2021-11-04 14:53:24 -0300

    Allow adding and download of actual files
    This adds a FileManager for getting metadata from files and for zipping all files. The single zip file can then be downloaded from the webserver.

--- app/src/main/java/org/onionshare/android/ui/MainViewModel.kt
+++ app/src/main/java/org/onionshare/android/ui/MainViewModel.kt
@@ -1,41 +1,71 @@
 package org.onionshare.android.ui
 
+import android.app.Application
+import android.net.Uri
+import android.text.format.Formatter
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import org.onionshare.android.OnionShareApp
-import org.onionshare.android.server.SendFile
+import org.onionshare.android.files.FileManager
 import org.onionshare.android.server.SendPage
 import org.onionshare.android.server.WebserverManager
+import org.slf4j.LoggerFactory
 import javax.inject.Inject
+
+private val LOG = LoggerFactory.getLogger(MainViewModel::class.java)
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    app: OnionShareApp,
+    private val app: Application,
     handle: SavedStateHandle,
     private val webserverManager: WebserverManager,
+    private val fileManager: FileManager,
 ) : AndroidViewModel(app) {
 
+    private val _fileManagerState = MutableStateFlow<FileManager.State>(FileManager.State.NoFiles)
+    val fileManagerState: StateFlow<FileManager.State> = _fileManagerState
     val webserverState = webserverManager.state
 
-    fun startServer() {
-        val sendPage = SendPage(
-            fileName = "foo.zip",
-            fileSize = "202590",
-            fileSizeHuman = "197.8 KiB"
-        ).apply {
-            // the below will get constructed programmatically later, this is just an example
-            addFiles(
-                listOf(
-                    SendFile("foo", "1 KiB"),
-                    SendFile("bar", "42 MiB"),
-                )
-            )
-        }
+    override fun onCleared() {
+        super.onCleared()
+        // TODO later, we shouldn't stop the server when closing the activity
+        stopServer()
+    }
+
+    fun onUrisReceived(uris: List<Uri>) {
+        if (uris.isEmpty()) return // user backed out of select activity
+
+        // not supporting selecting entire folders with sub-folders
         viewModelScope.launch(Dispatchers.IO) {
+            val filesAdded = fileManager.addFiles(uris)
+            _fileManagerState.value = filesAdded
+        }
+    }
+
+    fun startServer() {
+        // FIXME this is a mixing of concerns to get the right state in the UI
+        webserverManager.onFilesBeingZipped()
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val filesAdded = fileManagerState.value as FileManager.State.FilesAdded
+            val filesReady = fileManager.zipFiles(filesAdded)
+            _fileManagerState.value = filesReady
+
+            LOG.error("$filesReady")
+            val fileSize = filesReady.zip.length()
+            val sendPage = SendPage(
+                fileName = "download.zip",
+                fileSize = fileSize.toString(),
+                fileSizeHuman = Formatter.formatShortFileSize(app.applicationContext, fileSize),
+                zipFile = filesReady.zip,
+            ).apply {
+                addFiles(filesReady.files)
+            }
             webserverManager.start(sendPage)
         }
     }
