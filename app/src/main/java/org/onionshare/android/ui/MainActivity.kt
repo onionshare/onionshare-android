e785aa6d9c12f4fd535e99ad17fcba0319afb32a
Torsten Grote <t@grobox.de>
2023-03-29 09:56:20 -0300

    Target Android SDK 33
    and request notification permission due to it

--- app/src/main/java/org/onionshare/android/ui/MainActivity.kt
+++ app/src/main/java/org/onionshare/android/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package org.onionshare.android.ui
 
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.activity.viewModels
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,10 +16,22 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private val requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
+        // we don't care if the user wants to see notifications or not
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MainUi(viewModel)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // ask for notification permission right away since we already ask for doze exception when pressing start
+        if (SDK_INT >= 33 && checkSelfPermission(this, POST_NOTIFICATIONS) != PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(POST_NOTIFICATIONS)
         }
     }
 }
