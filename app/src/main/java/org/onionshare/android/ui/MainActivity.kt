d219f5f3165773efafadbd675e3f2e30dbf007b0
Torsten Grote <t@grobox.de>
2023-08-15 14:46:26 +0200

    Allow sharing files via Android's share sheet

--- app/src/main/java/org/onionshare/android/ui/MainActivity.kt
+++ app/src/main/java/org/onionshare/android/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package org.onionshare.android.ui
 
 import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.Intent
+import android.content.Intent.EXTRA_STREAM
+import android.net.Uri
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
@@ -25,6 +29,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainUi(viewModel)
         }
+        onNewIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_SEND -> {
+                handleSend(intent.getParcelableExtra<Parcelable>(EXTRA_STREAM) as? Uri)
+            }
+
+            Intent.ACTION_SEND_MULTIPLE -> {
+                handleSendMultiple(intent.getParcelableArrayListExtra(EXTRA_STREAM))
+            }
+
+            else -> super.onNewIntent(intent)
+        }
+    }
+
+    private fun handleSend(uri: Uri?) {
+        if (uri == null) return
+        viewModel.onUrisReceived(listOf(uri), false)
+    }
+
+    private fun handleSendMultiple(parcelables: ArrayList<Parcelable>?) {
+        if (parcelables == null) return
+        val uris = parcelables.mapNotNull { it as? Uri }
+        if (uris.isEmpty()) return
+        viewModel.onUrisReceived(uris, false)
     }
 
     override fun onStart() {
