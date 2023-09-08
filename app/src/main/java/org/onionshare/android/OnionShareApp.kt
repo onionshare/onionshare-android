73d39805c4bb447748f9a6f5e342482c7090bf68
akwizgran <michael@briarproject.org>
2023-09-08 14:48:47 +0100

    Remove unused code.

--- app/src/main/java/org/onionshare/android/OnionShareApp.kt
+++ app/src/main/java/org/onionshare/android/OnionShareApp.kt
@@ -1,14 +1,11 @@
 package org.onionshare.android
 
 import android.app.Activity
-import android.app.ActivityManager
 import android.app.Application
 import android.app.Application.ActivityLifecycleCallbacks
 import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
 import android.content.UriPermission
-import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
-import android.os.Process
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.os.StrictMode.VmPolicy
@@ -27,21 +24,10 @@ class OnionShareApp @Inject constructor() : Application(), ActivityLifecycleCall
         if (BuildConfig.DEBUG) enableStrictMode()
         super.onCreate()
         registerActivityLifecycleCallbacks(this)
-        if (!isTorProcess()) releaseUriPermissions()
+        releaseUriPermissions()
         // Route java.util.logging through Logback
         SLF4JBridgeHandler.removeHandlersForRootLogger()
         SLF4JBridgeHandler.install()
-    }
-
-    private fun isTorProcess(): Boolean {
-        val processName = if (SDK_INT >= 28) {
-            getProcessName()
-        } else {
-            val pid = Process.myPid()
-            val manager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
-            manager.runningAppProcesses?.firstOrNull { it.pid == pid }?.processName ?: return false
-        }
-        return processName.endsWith(":tor")
     }
 
     /**
